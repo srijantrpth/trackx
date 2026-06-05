6d814a793446503273183405f2f60a77690ed897
srijantrpth <srijantrpth@gmail.com>
2026-06-05 23:31:22 +0530

    Add Owner Field

--- tracker/models.py
+++ tracker/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     def __str__(self):
         return self.name
 
