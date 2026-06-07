bd68415278e2684f51661b967fccb0394eae1c07
srijantrpth <srijantrpth@gmail.com>
2026-06-07 23:06:04 +0530

    Add .env

---
+++ .env.py
@@ -0,0 +1,3 @@
+SECRET_KEY=django-insecure-your-very-long-secret-key
+DEBUG=True
+REDIS_URL=redis://default:6KUBa7AYbHH3QkHmiYTyRCyZO3xuKnfZ@redis-16762.crce263.ap-south-1-1.ec2.cloud.redislabs.com:16762
