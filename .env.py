c3d2a99b19d4c359d9ccf9926493a436c68c7606
Srijan Tripathi <srijantrpth@gmail.com>
2026-06-10 19:39:12 +0000

    Remove redundant file

--- .env.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY=django-insecure-your-very-long-secret-key
-DEBUG=True
-REDIS_URL=redis://default:6KUBa7AYbHH3QkHmiYTyRCyZO3xuKnfZ@redis-16762.crce263.ap-south-1-1.ec2.cloud.redislabs.com:16762
